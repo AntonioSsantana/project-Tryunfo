f6a912b070e8cbb4f92893ff8f1ff117be005c1e
Antônio Santana <antonionetosg6@gmail.com>
2022-08-29 11:43:58 -0300

    Requisito #1 [LINT]

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,18 +4,30 @@ class Form extends React.Component {
   render() {
     return (
       <form>
-        <input type="text" data-testid="name-input" />
+        <label htmlFor="name">
+          <input type="text" name="name" data-testid="name-input" />
+        </label>
         <textarea cols="30" rows="10" data-testid="description-input" />
-        <input type="number" data-testid="attr1-input" />
-        <input type="number" data-testid="attr2-input" />
-        <input type="number" data-testid="attr3-input" />
-        <input type="text" data-testid="image-input" />
+        <label htmlFor="attr1">
+          <input type="number" name="attr1" data-testid="attr1-input" />
+        </label>
+        <label htmlFor="attr2">
+          <input type="number" name="attr2" data-testid="attr2-input" />
+        </label>
+        <label htmlFor="attr3">
+          <input type="number" name="attr3" data-testid="attr3-input" />
+        </label>
+        <label htmlFor="input-image">
+          <input type="text" name="input-image" data-testid="image-input" />
+        </label>
         <select name="" id="">
           <option value="normal">normal</option>
           <option value="raro">raro</option>
           <option value="muito raro">muito raro</option>
         </select>
-        <input type="checkbox" data-testid="trunfo-input" />
+        <label htmlFor="trunfo">
+          <input type="checkbox" name="trunfo" data-testid="trunfo-input" />
+        </label>
         <button type="button" data-testid="save-button">Salvar</button>
       </form>
     );
