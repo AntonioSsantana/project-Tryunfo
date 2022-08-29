0ecec49291d65e4975b49bef2a956265335adfef
Antônio Santana <antonionetosg6@gmail.com>
2022-08-29 11:53:18 -0300

    Teste 1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,28 +5,28 @@ class Form extends React.Component {
     return (
       <form>
         <label htmlFor="name">
-          <input type="text" name="name" data-testid="name-input" />
+          <input type="text" id="name" data-testid="name-input" />
         </label>
         <textarea cols="30" rows="10" data-testid="description-input" />
         <label htmlFor="attr1">
-          <input type="number" name="attr1" data-testid="attr1-input" />
+          <input type="number" id="attr1" data-testid="attr1-input" />
         </label>
         <label htmlFor="attr2">
-          <input type="number" name="attr2" data-testid="attr2-input" />
+          <input type="number" id="attr2" data-testid="attr2-input" />
         </label>
         <label htmlFor="attr3">
-          <input type="number" name="attr3" data-testid="attr3-input" />
+          <input type="number" id="attr3" data-testid="attr3-input" />
         </label>
         <label htmlFor="input-image">
-          <input type="text" name="input-image" data-testid="image-input" />
+          <input type="text" id="input-image" data-testid="image-input" />
         </label>
         <select name="" id="">
           <option value="normal">normal</option>
           <option value="raro">raro</option>
           <option value="muito raro">muito raro</option>
         </select>
-        <label htmlFor="trunfo">
-          <input type="checkbox" name="trunfo" data-testid="trunfo-input" />
+        <label htmlFor="trunfo-input">
+          <input type="checkbox" id="trunfo-input" data-testid="trunfo-input" />
         </label>
         <button type="button" data-testid="save-button">Salvar</button>
       </form>
