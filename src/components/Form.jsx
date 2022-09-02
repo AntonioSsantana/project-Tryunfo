01c4903145c2407453147f7f58d712ad2a8c5830
Antônio Santana <antonionetosg6@gmail.com>
2022-09-02 15:37:07 -0300

    Requisito 1 refeito

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,1 +1,87 @@
+import React from "react";
 
+class Form extends React.Component {
+  render() {
+
+    return (
+      <main>
+        <label htmlFor="name">
+          Nome
+          <input
+            name="name"
+            type="text"
+            data-testid="name-input"
+          />
+        </label>
+        <label htmlFor="description">
+          Descrição
+          <textarea
+            name="description"
+            cols="30"
+            rows="10"
+            data-testid="description-input"
+          />
+        </label>
+        <label htmlFor="attr1">
+          Físico
+          <input
+            name="attr1"
+            type="number"
+            data-testid="attr1-input"
+          />
+        </label>
+        <label htmlFor="attr2">
+          Mágico
+          <input
+            type="number"
+            name="attr2"
+            data-testid="attr2-input"
+          />
+        </label>
+        <label htmlFor="attr3">
+          Velocidade
+          <input
+            type="number"
+            name="attr3"
+            data-testid="attr3-input"
+          />
+        </label>
+        <label htmlFor="img">
+          Imagem
+          <input
+            name="img"
+            type="image"
+            src=""
+            alt=""
+            data-testid="image-input"
+          />
+        </label>
+        <label htmlFor="rarity">
+          Raridade
+          <select name="rarity" data-testid="rare-input">
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
+          </select>
+        </label>
+        <label htmlFor="isTrunfo">
+          IsTrunfo?
+          <input 
+          type="checkbox" 
+          name="isTrunfo"  
+          data-testid="trunfo-input"
+          />
+        </label>
+        <label htmlFor="save">
+          <button 
+          name="save"
+          type="submit" 
+          data-testid="save-button"
+          />
+        </label>
+      </main>
+    )
+  }
+}
+
+export default Form;
